b69bc04592bf14c06560439a4292df6aa561920c
Matheus-Mannarino <mths.mannarino@gmail.com>
2026-05-16 10:21:31 -0300

    admin

--- src/AAC/core/models.py
+++ src/AAC/core/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Usuario(AbstractUser):
+
+    class Perfil(models.TextChoices):
+        ALUNO = 'ALUNO', 'Aluno'
+        COORDENADOR = 'COORDENADOR', 'Coordenador'
+        ORG = 'ORG', 'Organização Acadêmica'
+
+    perfil = models.CharField(
+        max_length=20,
+        choices=Perfil.choices
+    )
