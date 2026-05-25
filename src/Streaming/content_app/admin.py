c0319646c51e574ac76e951956c941dc26f5d0da
Matheus Mannarino <mths.mannarino@gmail.com>
2026-05-25 17:25:32 -0300

    inicio_autenticacao

--- src/Streaming/content_app/admin.py
+++ src/Streaming/content_app/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 class ContentAdmin(admin.ModelAdmin):
     list_display = ('title', 'content_type', 'is_public')
     list_filter = ('content_type', 'is_public', 'status')
+    search_fields = ('title',)
 
 
 admin.site.register(models.Content, ContentAdmin)
