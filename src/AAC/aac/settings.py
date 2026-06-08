1d24b8fa352913e04d4431448bae0843329b6cb7
Matheus Mannarino <mths.mannarino@gmail.com>
2026-06-08 16:17:27 -0300

    CorsOrigin

--- src/AAC/aac/settings.py
+++ src/AAC/aac/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'drf_spectacular',
     'core',
 ]
@@ -47,6 +48,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+#CORS_ALLOWED_ORIGINS = [
+       #"http://127.0.0.1:8000/",
+       #"http://localhost:3000",
+#]
+
+CORS_ALLOW_ALL_ORIGINS = False
 
 ROOT_URLCONF = 'aac.urls'
 
